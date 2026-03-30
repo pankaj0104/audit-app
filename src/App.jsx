0f6a9f2bad158f7ebead64c1aa8ebe888d68ac3e
pankaj0104 <66308535+pankaj0104@users.noreply.github.com>
2026-03-30 22:40:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
-    <div style={{ padding: 20, background: "#111827", minHeight: "100vh", color: "white" }}>
+    <div style={{ padding: 20, background: "#111827", minHeight: "100vh", color: "white", fontFamily: "Arial" }}>
       <h1>NEW DASHBOARD BUILD LOADED 🚀</h1>
+      <p>Ab sahi file chal rahi hai.</p>
     </div>
   );
 }
